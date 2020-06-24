beabc87ff1a79e68900de6c4bf2fbf65ef3d54fa
SarahTaaherBonna <sarahtb25@gmail.com>
2020-06-24 23:38:34 +0800

    Fix conflicts

--- components/CreditCard.js
+++ components/CreditCard.js
@@ -47,7 +47,7 @@ export default class CreditCard extends Component {
   };
 
   onPressSubmit = async () => {
-    console.log(this.state.cardName)
+    console.log(this.state.cardName);
     try {
       const tokenObject = firebaseSDK.getToken();
       // console.log(Object.values(token));
