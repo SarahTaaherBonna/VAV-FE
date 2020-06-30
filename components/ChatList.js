a8cb012baee415655545542469632f5d07cc1a95
khanh <khanh.phungntu@gmail.com>
2020-06-30 12:37:46 +0800

    Fix try catch

--- components/ChatList.js
+++ components/ChatList.js
@@ -97,8 +97,9 @@ export default class ChatList extends Component {
   }
 
   componentDidMount() {
-    this.setState({loading: true})
+
     firebaseSDK.getChatList(async (chatKey, merchantname, buyername, merchantuid, buyeruid, text) => {
+      this.setState({loading: true})
       let uri;
       if (firebaseSDK.uid == buyeruid){
         uri = await firebaseSDK.getChatAvatar(merchantuid);
